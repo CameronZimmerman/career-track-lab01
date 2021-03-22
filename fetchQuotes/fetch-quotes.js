6e35caab7de7acdc65744537a5be870feffa5ec9
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-22 14:52:17 -0700

    add mock fetch to test

--- fetchQuotes/fetch-quotes.js
+++ fetchQuotes/fetch-quotes.js
@@ -1,4 +1,20 @@
+jest.mock('node-fetch');
 const fetch = require('node-fetch');
+
+fetch.mockImplementation(() => {
+  return Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        {
+          character: 'Bender',
+          quote:
+            "It's always so sad when a friend goes crazy and you have a have a big clambake\nand cook him! Yee-ha!", //eslint-disable-line
+          image:
+            'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png',
+        },
+      ]),
+  });
+});
 
 const getQuotes = async () => {
   const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes/1');
@@ -6,8 +22,8 @@ const getQuotes = async () => {
   return {
     name: body[0].character,
     text: body[0].quote,
-    image: body[0].image
-  }
+    image: body[0].image,
+  };
 };
 
 module.exports = getQuotes;
