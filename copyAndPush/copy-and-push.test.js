6329c0acf06852b0bd41355aee35afd05d8844b6
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-22 13:39:20 -0700

    make sure it is a copy

--- copyAndPush/copy-and-push.test.js
+++ copyAndPush/copy-and-push.test.js
@@ -1,10 +1,16 @@
 const copyAndPush = require('./copy-and-push');
 
 describe('copyAndPush array function', () => {
-  it('should take an array and an item then return a copy with the new item pushed', () => {
+  it('should take an array and an item then return the array with the new item pushed', () => {
     const numbers = [0, 1, 2];
-    const newNumbers = copyAndPush(numbers, 3);
+    copyAndPush(numbers, 3);
 
-    expect(newNumbers).toEqual([0, 1, 2, 3]);
+    expect(numbers).toEqual([0, 1, 2, 3]);
+  });
+  it('should take an array and an item then return a copy of the array with the new item pushed', () => {
+    const numbers = [1, 2, 3];
+    const newNumbers = copyAndPush(numbers, 4);
+
+    expect(newNumbers).toEqual([1, 2, 3, 4]);
   });
 });
