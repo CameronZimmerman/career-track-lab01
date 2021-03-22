c05fe293bdcbc02fb41aad6e94c0cac3fab97166
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-22 16:53:37 -0700

    revert back to original test and add env file

--- fetchQuotes/fetch-quotes.js
+++ fetchQuotes/fetch-quotes.js
@@ -1,9 +1,29 @@
-const getQuotes = async (data) => {
+jest.mock('node-fetch');
+const fetch = require('node-fetch');
+
+fetch.mockImplementation(() => {
+  return Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        {
+          character: 'Bender',
+          quote:
+            "It's always so sad when a friend goes crazy and you have a have a big clambake\nand cook him! Yee-ha!", //eslint-disable-line
+          image:
+            'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png',
+        },
+      ]),
+  });
+});
+
+const fetchQuotes = async () => {
+  const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes/1');
+  const body = await res.json();
   return {
-    name: data.character,
-    text: data.quote,
-    image: data.image,
+    name: body[0].character,
+    text: body[0].quote,
+    image: body[0].image,
   };
 };
 
-module.exports = getQuotes;
+module.exports = fetchQuotes;
--- fetchQuotes/fetch-quotes.test.js
+++ fetchQuotes/fetch-quotes.test.js
@@ -1,30 +1,9 @@
-const getQuotes = require('./fetch-quotes');
-
-jest.mock('node-fetch');
-const fetch = require('node-fetch');
-
-fetch.mockImplementation(() => {
-  return Promise.resolve({
-    json: () =>
-      Promise.resolve([
-        {
-          character: 'Bender',
-          quote:
-            "It's always so sad when a friend goes crazy and you have a have a big clambake\nand cook him! Yee-ha!", //eslint-disable-line
-          image:
-            'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png',
-        },
-      ]),
-  });
-});
+const fetchQuotes = require('./fetch-quotes');
 
 describe('getQuotes function', () => {
   it('should return a single quote in the correct format', async () => {
-    const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes/1');
-    const body = await res.json();
+    const quote = await fetchQuotes();
 
-    const quote = await getQuotes(body[0]);
-    
     const shape = {
       name: expect.any(String),
       text: expect.any(String),
