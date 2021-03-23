5619b10628c203e74d26bfe20ca78d1ea9e0a1a4
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-23 08:47:24 -0700

    refactor mock fetch

--- fetchQuotes/fetch-quotes.js
+++ fetchQuotes/fetch-quotes.js
@@ -1,22 +1,3 @@
-if(process.env.NODE_ENV === 'test') {
-  jest.mock('node-fetch');
-  const fetch = require('node-fetch');
-
-  fetch.mockImplementation(() => {
-    return Promise.resolve({
-      json: () =>
-        Promise.resolve([
-          {
-            character: 'Bender',
-            quote:
-              "It's always so sad when a friend goes crazy and you have a have a big clambake\nand cook him! Yee-ha!", //eslint-disable-line
-            image:
-              'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png',
-          },
-        ]),
-    });
-  });
-}
 const fetch = require('node-fetch');
 
 const fetchQuotes = async () => {
--- fetchQuotes/fetch-quotes.test.js
+++ fetchQuotes/fetch-quotes.test.js
@@ -1,4 +1,21 @@
 const fetchQuotes = require('./fetch-quotes');
+jest.mock('node-fetch');
+const fetch = require('node-fetch');
+
+fetch.mockImplementation(() => {
+  return Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        {
+          character: 'Bender',
+          quote:
+            "It's always so sad when a friend goes crazy and you have a have a big clambake\nand cook him! Yee-ha!", //eslint-disable-line
+          image:
+            'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png',
+        },
+      ]),
+  });
+});
 
 describe('getQuotes function', () => {
   it('should return a single quote in the correct format', async () => {
